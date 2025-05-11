c8cbcbb54406c787acc9cf1bc288d814c9c3fb93
ryanshanker27 <theryanshanker@gmail.com>
2025-05-11 15:19:49 -0400

    Added API log

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+console.log("Starting app; API_BASE =", import.meta.env.VITE_API_URL);
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
